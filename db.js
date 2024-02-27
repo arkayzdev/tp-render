788e34a2e4593bff906b0a1f8a71f8a0a0cdf8c1
Franck Zhuang <franck.zhuang@hotmail.com>
2024-02-27 15:07:41 +0100

    Add render.yaml

--- db.js
+++ db.js
@@ -2,11 +2,12 @@ const { Sequelize } = require('sequelize')
 
 // database
 const sequelize = new Sequelize(
-  process.env.DB_NAME, // Database name
-  process.env.USER, // User
-  process.env.PASSWORD, // Password
+  // process.env.DB_NAME, // Database name
+  // process.env.USER, // User
+  // process.env.PASSWORD, // Password
+  process.env.DB_URL
   {
-    host: process.env.HOST, // Host
+    // host: process.env.HOST, // Host
     dialect: 'postgres',
     dialectOptions: {
       ssl: {
